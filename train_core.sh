1417d3ab55640de712d5664d0c8e644fc1fac53e
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-05-06 21:28:12 -0400

    fix(training): use docker command instead of docker-compose

--- train_core.sh
+++ train_core.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-echo "Training model..."
-# Get env variables from 'local.env' file
-export $(cat local.env | grep -v ^# | xargs)
+echo "Training core model..."
 # not removing first provokes a "resource unavailable" error on the folder when persisting
 rm -rf models/core/*
-docker-compose stop core
 # train
-docker-compose run core train \
-    -s project/stories/ \
+docker run \
+    -v /$(pwd)/models/core:/app/model \
+    -v /$(pwd)/project:/app/project \
+    -t botfront/rasa-core-bf train \
     -d project/domains/domain.yml \
-    -c project/policies.yml \
-    -o model
+    -o model \
+    -s project/stories \
+    -c project/policies.yml 
 
 # Zip model to upload
 cd models/core
 zip -r model-$(date -u +"%Y-%m-%dT%H:%M:%SZ").zip *
 cd ..
-docker-compose start core
